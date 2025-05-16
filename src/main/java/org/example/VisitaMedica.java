3b6eb9b6099db2cdf2302f52c8b0bf9a13ffc2a1
nicola-filannino <filannino.nicola9@gmail.com>
2025-05-16 15:59:46 +0200

    Classe VisitaMedica

--- src/main/java/org/example/VisitaMedica.java
+++ src/main/java/org/example/VisitaMedica.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class VisitaMedica {
+    private LocalDate data;
+    private LocalTime orario;
+    private Dottore dottore;
+    private Diagnosi diagnosi;
+    private boolean confermata;
+
+    public VisitaMedica(LocalDate data, LocalTime orario, Dottore dottore) {
+        this.data = data;
+        this.orario = orario;
+        this.dottore = dottore;
+        this.confermata = false;
+    }
+
+    public void conferma() {
+        this.confermata = true;
+    }
+
+    public void assegnaDiagnosi(Diagnosi diagnosi) {
+        this.diagnosi = diagnosi;
+    }
+
+    @Override
+    public String toString() {
+        return "Visita il " + data + " alle " + orario +
+                " con " + dottore +
+                (confermata ? " [CONFERMATA]" : " [NON CONFERMATA]") +
+                (diagnosi != null ? " - Diagnosi: " + diagnosi : "");
+    }
 }
